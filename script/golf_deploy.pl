595e0cb0dc08c4bf9d3ac654c519d9e9c681a948
Andreas Marienborg <andreas.marienborg@gmail.com>
2009-07-28 23:29:52 +0200

    Better deploy script

--- script/golf_deploy.pl
+++ script/golf_deploy.pl
@@ -4,12 +4,25 @@ use Config::Any;
 
 use lib qw(lib);
 
-my $cfg = Config::Any->load_files({ files => [qw/golf.yml/], use_ext => 1})
-->[0]->{'golf.yml'}->{'Model::Kioku'};
-
 use Data::Dump qw/dump/;
+
+my $cfg = Config::Any->load_files({ files => [qw/golf.yml golf_local.yml/], use_ext => 1});
+
+
+my $dsn;
+
+foreach (@$cfg) {
+    if ($_->{'golf.yml'} and !$dsn) {
+        # not seen local, setting $dsn
+        $dsn = $_->{'golf.yml'}->{'Model::Kioku'};
+    } elsif ($_->{'golf_local.yml'}) {
+        $dsn = $_->{'golf_local.yml'}->{'Model::Kioku'} if $_->{'golf_local.yml'}->{'Model::Kioku'};
+    }
+}
+
+
 
 use Golf::Domain;
 
 
-my $d = Golf::Domain->new(%$cfg, extra_args => { create => 1 } );
+my $d = Golf::Domain->new(%$dsn, extra_args => { create => 1 } );
