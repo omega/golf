4b7101e9d4d27e583b3a1089c2515f03b6a32f9c
Andreas Marienborg <andreas.marienborg@gmail.com>
2009-08-21 21:12:59 +0200

    Bleh, messed up "YAML-loading in BASH" somewhat :p

--- script/db_dump.sh
+++ script/db_dump.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DSN=`cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }'`
+DSN=`cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }' | sed 's/"//g'`
 
 FILE=db.yml
 if [ -n "$1" ]; then
--- script/reset_db.sh
+++ script/reset_db.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-DSN=`cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }'`
-DIR=` cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }' | sed 's/bdb-gin:dir=//'`
+DSN=`cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }' | sed 's/"//g'`
+DIR=` cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }' | sed 's/bdb-gin:dir=//' | sed 's/"//g'`
+echo "reseting : $DSN in $DIR"
+
 rm -rf $DIR
 ./script/golf_deploy.pl
 if [ -z "$1" ]; then
