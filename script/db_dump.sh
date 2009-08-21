40f8b2c716554f9a14421f5bdb22698f529290f9
Andreas Marienborg <andreas.marienborg@gmail.com>
2009-08-21 20:42:32 +0200

    make db_dump.sh and reset_db.sh config-aware, to some extent

--- script/db_dump.sh
+++ script/db_dump.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+DSN=`cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }'`
+
 FILE=db.yml
 if [ -n "$1" ]; then
     FILE=$1
 fi
-perl -MDateTime::Format::Strptime `which kioku` dump -D 'bdb-gin:dir=db/' --file $FILE --force
+perl -MDateTime::Format::Strptime `which kioku` dump -D $DSN --file $FILE --force
--- script/reset_db.sh
+++ script/reset_db.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-rm -rf db/
+DSN=`cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }'`
+DIR=` cat golf.yml golf_local.yml | grep dsn | tail -n 1 | awk '{ print $2 }' | sed 's/bdb-gin:dir=//'`
+rm -rf $DIR
 ./script/golf_deploy.pl
 if [ -z "$1" ]; then
     echo "loading db.yml"
-    kioku load -D 'bdb-gin:dir=db/' --file db.yml
+    kioku load -D $DSN --file db.yml
 fi
